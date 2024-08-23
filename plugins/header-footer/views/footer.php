31206824d4fe3443e0f93c788c0d188308cfb44a
MDVR9980 <mdvahhabrajaee@gmail.com>
2024-08-24 02:16:19 +0330

    Updated

--- plugins/header-footer/views/footer.php
+++ plugins/header-footer/views/footer.php
@@ -1,7 +1,6 @@
 <div class="col-md-10 mx-auto p-4">
 	<center>	
 		<h3>Footer</h3>
-		<div>Copyright <?=date("Y")?></div>
 	</center>
 </div>
 	<script src="<?=ROOT?>/assets/js/bootstrap.min.js"></script>
